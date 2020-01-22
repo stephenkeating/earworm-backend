06754a4ceb33ed0cf8565bfb5ea276dc7c586ca9
Stephen Keating <stephen.keating@gmail.com>
2020-01-22 10:52:22 -0500

    created high scores method on playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,8 @@
 class Playlist < ApplicationRecord
   has_many :tracks, dependent: :delete_all
+
+  def high_scores
+    Game.all.where.not(user: nil).where("playlist_id": self.id).sort_by { |game| -game.score}.map { |game| {user: game.user, score: game.score}}
+  end
+
 end
--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,4 +1,4 @@
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :spotify_id
+  attributes :id, :name, :spotify_id, :high_scores
   has_many :tracks
 end
