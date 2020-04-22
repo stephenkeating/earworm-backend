cad59122853e2282230f75ae8468d1d62471b798
Stephen Keating <stephen.keating@gmail.com>
2020-04-22 15:03:07 -0400

    updated cors to include earworm.netlify.app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://earworm.netlify.com/'
+    origins 'https://earworm.netlify.com/', 'https://earworm.netlify.app'
 
     resource '*',
       headers: :any,
