c6ebcb73658043f5eee676b6a82e63608731e8f2
Stephen Keating <stephen.keating@gmail.com>
2020-05-12 16:53:29 -0400

    added aws amplify to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://earworm.netlify.com/', 'https://earworm.netlify.app'
+    origins 'https://earworm.netlify.com/', 'https://earworm.netlify.app', 'https://master.d3ojzg0sgryvyv.amplifyapp.com'
 
     resource '*',
       headers: :any,
