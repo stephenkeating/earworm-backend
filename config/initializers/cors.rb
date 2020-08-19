524955a71324fd2b9cb54b0aeabc0e6ada6e9e4e
Stephen Keating <stephen.keating@gmail.com>
2020-08-19 15:07:32 -0400

    restrict cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'https://earworm.netlify.com/', 'https://earworm.netlify.app', 'https://master.d3ojzg0sgryvyv.amplifyapp.com'
-    origins '*'
+    origins 'https://master.d3ojzg0sgryvyv.amplifyapp.com'
+    # origins '*'
 
     resource '*',
       headers: :any,
